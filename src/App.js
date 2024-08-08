5fea3e2cf4778b863149a20488da1100484c861c
abdullah956 <abdullaharshed956@gmail.com>
2024-08-08 13:11:27 +0500

    star-rating-3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/*Accordian Component*/}
-      <Accordian />
+      {/* <Accordian /> */}
       {/*Random Color Component*/}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      {/*StarRating Component*/}
+      <StarRating/>
     </div>
   );
 }
