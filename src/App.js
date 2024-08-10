7d8ebb9c317784f91e92a1272c123710c8eca080
abdullah956 <abdullaharshed956@gmail.com>
2024-08-10 10:29:18 +0500

    load-more-data-5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 function App() {
   return (
     <div className="App">
@@ -13,11 +14,13 @@ function App() {
       {/*StarRating Component*/}
       {/* <StarRating/> */}
       {/*ImageSlider Component*/}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      {/* Load more products component */}
+      <LoadMoreData/>
     </div>
   );
 }
