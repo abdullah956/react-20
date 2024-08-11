66602adb8b86cc48ce6bd270d3a551d0f8c627c8
abdullah956 <abdullaharshed956@gmail.com>
2024-08-11 11:25:02 +0500

    tree-view-6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 function App() {
   return (
     <div className="App">
@@ -20,7 +22,9 @@ function App() {
         limit={"10"}
       /> */}
       {/* Load more products component */}
-      <LoadMoreData/>
+      {/* <LoadMoreData/> */}
+      {/* Tree view component */}
+      <TreeView menus={menus} />
     </div>
   );
 }
