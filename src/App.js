1fc3656a6508b0cdaea851c74252db13bdca9492
abdullah956 <abdullaharshed956@gmail.com>
2024-08-12 10:18:01 +0500

    qr-code-generator-7

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
+import QRCodeGenerator from "./components/qr-code-generator";
 function App() {
   return (
     <div className="App">
@@ -24,7 +25,9 @@ function App() {
       {/* Load more products component */}
       {/* <LoadMoreData/> */}
       {/* Tree view component */}
-      <TreeView menus={menus} />
+      {/* <TreeView menus={menus} /> */}
+      {/* QR code generator */}
+      <QRCodeGenerator/>
     </div>
   );
 }
